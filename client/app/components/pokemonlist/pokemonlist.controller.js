61aa8bcb83563dd4e4387e10bcc6fa3b91756050
goncalo.costa <goncalo.costa@inw-tech.com>
2016-09-07 21:18:39 +0100

    Building the Pokemon list page and respective components

--- client/app/components/pokemonlist/pokemonlist.controller.js
+++ client/app/components/pokemonlist/pokemonlist.controller.js
@@ -1,7 +1,27 @@
 class PokemonlistController {
+
   constructor() {
     this.name = 'pokemonlist';
+    this.pokemons = [];
   }
+
+  getMore () {
+    for(var i = 0; i<12; i++){
+      this.pokemons.push({
+        "name": "bulbasaur",
+        "id": "001",
+        "types": [
+          { "name": "grass" },
+          { "name": "poison" }
+        ]
+      });
+    }
+  }
+
+  $onInit() {
+    this.getMore();
+  }
+
 }
 
-export default PokemonlistController;
+export default PokemonlistController; 
